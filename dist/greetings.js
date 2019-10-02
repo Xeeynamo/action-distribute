c503381d788afcaf3ebcfa8f5ed00c82499c9685
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-10-02 19:14:40 +0100

    Attempt to fix permission error

--- dist/greetings.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getGreeting(whoToGreet) {
-    if (!whoToGreet || whoToGreet == "")
-        whoToGreet = "world";
-    return `Hello ${whoToGreet}!`;
-}
-exports.getGreeting = getGreeting;
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -9,23 +18,46 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const github = __importStar(require("@actions/github"));
-const greetings = __importStar(require("./greetings"));
-try {
-    const nameToGreet = core.getInput("who-to-greet");
-    core.startGroup('getGreeting group...');
-    const greeting = greetings.getGreeting(nameToGreet);
-    core.endGroup();
-    core.exportVariable('CI', "true");
-    console.log(greeting);
-    core.setOutput('greeting', greeting);
-    console.log("I am logging from console");
-    console.warn("I am warning from console");
-    core.debug("I am logging from core.debug");
-    core.info("I am logging from core.info");
-    core.warning("I am logging from core.warning");
-    const context = JSON.stringify(github.context, undefined, 2);
-    console.log(`Context: ${context}`);
+const utils = __importStar(require("./gitutils"));
+function getLastAuthor() {
+    return github.context.payload.head_commit.author;
 }
-catch (error) {
-    core.setFailed(`Failed for the following reason: ${error.message}`);
+function getRepositorySsh() {
+    return github.context.payload.repository.ssh_url;
 }
+function getGithubToken() {
+    return process.env.GITHUB_TOKEN;
+}
+function getRepositorySshWithToken() {
+    const repoUri = getRepositorySsh();
+    const token = getGithubToken();
+    return repoUri.replace('git@', `https://x-access-token:${token}@`);
+}
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            core.exportVariable('CI', "true");
+            const distributionBranch = core.getInput("distribution-branch");
+            const isTypescript = core.getInput("is-typescript") == 'true';
+            const result = (yield utils.setAuthorName("Test name")) &&
+                (yield utils.setAuthorName("test@github.com")) &&
+                (yield utils.removeOrigin()) &&
+                (yield utils.addOrigin(getRepositorySshWithToken())) &&
+                (yield utils.createBranch(distributionBranch)) &&
+                (yield utils.npmInstall()) &&
+                (isTypescript == false || (isTypescript && (yield utils.tsc()))) &&
+                (yield utils.installNcc()) &&
+                (yield utils.ncc('./dist')) &&
+                (
+                // TODO: Remove everything but dist/ and action.yml
+                yield utils.commit("Distribute")) &&
+                (yield utils.push(distributionBranch));
+            if (result == false)
+                core.setFailed("One of the operations has failed.");
+        }
+        catch (error) {
+            core.setFailed(`Failed for the following reason: ${error.message}`);
+        }
+    });
+}
+run();
